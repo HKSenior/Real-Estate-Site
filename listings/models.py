1ee7752c322642062c22dd3efb0450589ba02a79
Hassani <hassanisnr@gmail.com>
2019-02-26 00:43:31 -0500

    Fixed internal server error when deleting realtors

--- listings/models.py
+++ listings/models.py
@@ -6,7 +6,11 @@ from realtors.models import Realtor
 
 
 class Listing(models.Model):
-    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    realtor = models.ForeignKey(
+        Realtor,
+        on_delete=models.DO_NOTHING,
+        db_constraint=False
+    )
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
